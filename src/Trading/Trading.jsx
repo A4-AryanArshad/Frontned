396d3103b3a9672e72b8c8e8a230a5786e752349
A4-AryanArshad <2021cs613@student.uet.edu.pk>
2025-06-19 11:01:28 +0500

    first commit

--- src/Trading/Trading.jsx
+++ src/Trading/Trading.jsx
@@ -1,20 +1,96 @@
 import React from 'react'
 import Header from '../Home/Header'
 import "./Trading.css";
+import Footer from '../Home/Footer';
 
 const Trading = () => {
   return (
     <>
-    <div id="upperheader">
-<Header/>
+      <div id="trade">
+        <Header />
+      </div>
+      <div id='logo2b'>
+        <div id="logp2a">
 
-<div id="coming">
-
-
-<h1>COMING SOON</h1>
-</div>
-
+          <img src="./Logo2.webp" />
+          <button>Coming Soon</button>
         </div>
+        <div id="logp2a2">
+          <h1>Blockchain-Based P2P Carbon Credits Marketplace</h1>
+          <img src="https://greenerinsights.com/wp-content/uploads/2024/12/Blockchain-Carbon-Market-768x452.webp" />
+        </div>
+        <div id="logo2a3">
+          <h1>Revolutionizing Carbon Emissions for a Sustainable Future</h1>
+          <div id="innerl1">
+            <div id="ary">
+              <h1>CARBON TRADING </h1>
+              <p>Unitok value from CRAw · FESZ · EU ETS, UK ETS, CH ETS  </p>
+              <p>COPS. A credits and EBC</p>
+
+            </div>
+            <img id="eweq" src="https://images.unsplash.com/photo-1580064461598-505b080a8242?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyYm9uJTIwZW1pc3Npb25zfGVufDB8fDB8fHww" />
+          </div>
+        </div>
+        <div id="thirdl">
+          <div id="f1">
+            <h1>The Leading Carbon Credits P2P Trading Platform</h1>
+            <h1>P2P Trading Platform</h1>
+          </div>
+          <div id="f2">
+            <h1>Environmental Market Data</h1>
+            <h1>at your Finger tips</h1>
+
+          </div>
+        </div>
+        <div id="marketC">
+          <div id="forthl">
+            <div id="ddu">
+              <div id="dd">
+                <button>*</button>
+                <h1>Caruso</h1>
+                <p>Garbase fications keller leppaterate</p>
+              </div>
+              <div id="dd">
+                <button>*</button>
+                <h1>Qaproc </h1>
+                <p>US Center vrride for olla</p>
+              </div>
+            </div>
+
+
+            <div id="ddu">
+              <div id="dd">
+                <button>*</button>
+                <h1>Qaptic </h1>
+                <p>Wede prollance fie flemogen pepponsumed</p>
+              </div>
+              <div id="dd">
+                <button>*</button>
+                <h1>Frobat sace fue </h1>
+                <p>Silexing lueu proentra</p>
+              </div>
+            </div>
+          </div>
+          <div id="market">
+            <h1>Market Data </h1>
+            <p>Imporoa monine you unil towiid pantom system with othen as dairy moosolvin ehiy ene to gnoth ethase centre finalichen of penilom opionainar and I randi motcibouss curcaloua </p>
+          </div>
+        </div>
+        <div id="covv">
+          <div id="logp2a2">
+            <h2>Fractioanl Investment Hub</h2>
+            <img src="https://www.rrfinance.com/Blogs/images/best-investment-options.png" />
+
+          </div>
+        </div>
+        <div id="texterareaq">
+          <h1>Democratizing Green Investment A New Paradigm</h1>
+          <p>The urgency of climate action has spurred a global reevcalclation of investment strategies, pushing sustainable finance from a niche consideration to a central pider diffronente inisource. Winlin this transformative staff, Fractionalmat. Ownership in Green Infrastructure stands silt as a particularly compelling innovation. In represure a departure from traditional investment models, which often limit participation to institutional giant’s and high-net worth individuals.
+          </p>
+        </div>
+        <Footer/>
+      </div>
+      
     </>
   )
 }
